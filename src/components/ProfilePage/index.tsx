f5623a4e0b82c87e5419acde240f29c20836f371
pupo <devpupo@outlook.com>
2021-08-24 00:17:11 -0300

    created the profile page and its style, applied the responsive breakpoint

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        {/* <EditButton outlined>Editar Perfil</EditButton> */}
+        <h1>Paulo Pupo</h1>
+        <h2>@pupos_</h2>
+
+        <p>
+          Developer at <a href="www.linkedin.com/in/devpupo">@Byte_.</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 27 de dezembro de 1998
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>102</strong>
+          </span>
+          <span>
+            <strong>2982 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
+

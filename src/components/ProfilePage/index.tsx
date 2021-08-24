c46722d067e3f2be945358a050b11b0a8ceddf0c
pupo <devpupo@outlook.com>
2021-08-24 00:52:45 -0300

    created feed

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Feed from '../Feed';
+
 import {
   Container,
   Banner,
@@ -8,7 +10,7 @@ import {
   LocationIcon,
   CakeIcon,
   Followage,
-  EditButton
+  EditButton,
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -47,9 +49,10 @@ const ProfilePage: React.FC = () => {
           </span>
         </Followage>
       </ProfileData>
+
+      <Feed />
     </Container>
   );
 };
 
 export default ProfilePage;
-
